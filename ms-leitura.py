dfc1c3b302eeeae3c1fa987e5d47e52f3af6eeb8
Rubens Levy Dourado <rubens.dourado@gmail.com>
2023-12-02 23:51:48 +0000

    Commit

---
+++ ms-leitura.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+import os
+import time
+from random import randrange
+
+import pika
+from dotenv import load_dotenv
+
+load_dotenv(verbose=True)
+
+RABBITMQ_URL      = os.getenv('RABBITMQ_URL')
+RABBITMQ_USERNAME = os.getenv('RABBITMQ_USERNAME')
+RABBITMQ_PASSWORD = os.getenv('RABBITMQ_PASSWORD')
+RABBITMQ_QUEUE    = os.getenv('RABBITMQ_QUEUE')
+
+connection = pika.BlockingConnection(
+  pika.ConnectionParameters(
+    host=RABBITMQ_URL,
+    credentials=pika.PlainCredentials(RABBITMQ_USERNAME, RABBITMQ_PASSWORD)
+  )
+)
+
+channel = connection.channel()
+
+channel.queue_declare(queue=RABBITMQ_QUEUE)
+
+def callback(ch, method, properties, body):
+    print(" [x] Received %r" % body)
+    time.sleep(randrange(0, 2))	
+    print(" [x] Done")
+    ch.basic_ack(delivery_tag = method.delivery_tag)
+
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(on_message_callback=callback, queue=RABBITMQ_QUEUE)
+
+print(' [*] Waiting for messages. To exit press CTRL+C')
+channel.start_consuming()
