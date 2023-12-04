6a06d12976015a0a302a1ab115e827aa1a088db7
Rubens Levy Dourado <rubens.dourado@gmail.com>
2023-12-04 23:08:21 +0000

    Prefetch Parameter

--- ms-leitura.py
+++ ms-leitura.py
@@ -13,6 +13,7 @@ RABBITMQ_URL      = os.getenv('RABBITMQ_URL')
 RABBITMQ_USERNAME = os.getenv('RABBITMQ_USERNAME')
 RABBITMQ_PASSWORD = os.getenv('RABBITMQ_PASSWORD')
 RABBITMQ_QUEUE    = os.getenv('RABBITMQ_QUEUE')
+RABBITMQ_PREFETCH = int(os.getenv('RABBITMQ_PREFETCH'))
 
 connection = pika.BlockingConnection(
   pika.ConnectionParameters(
@@ -27,11 +28,11 @@ channel.queue_declare(queue=RABBITMQ_QUEUE)
 
 def callback(ch, method, properties, body):
     print(" [x] Received %r" % body)
-    time.sleep(randrange(0, 2))	
+    time.sleep(randrange(0, 2))
     print(" [x] Done")
     ch.basic_ack(delivery_tag = method.delivery_tag)
 
-channel.basic_qos(prefetch_count=1)
+channel.basic_qos(prefetch_count=RABBITMQ_PREFETCH)
 channel.basic_consume(on_message_callback=callback, queue=RABBITMQ_QUEUE)
 
 print(' [*] Waiting for messages. To exit press CTRL+C')
